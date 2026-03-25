b2f48b97f80ec03b56507eb950b7ce393fe4d991
Veronika <nikavdv@gmail.com>
2026-03-25 17:35:48 +0100

    Delete tests/e2e/epam.client-work.spec.ts

--- tests/e2e/epam.client-work.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test.describe('EPAM client work', () => {
-  test('Explore Our Client Work displays Client Work', async ({ page }) => {
-    /* TEST_BODY */
-  });
-});
