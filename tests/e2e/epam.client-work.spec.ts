bd18c02a3f2a7bcc593e58fac52a5c9e96aad8ce
Veronika <nikavdv@gmail.com>
2026-03-25 17:35:02 +0100

    Create tests/e2e/epam.client-work.spec.ts

---
+++ tests/e2e/epam.client-work.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('EPAM client work', () => {
+  test('Explore Our Client Work displays Client Work', async ({ page }) => {
+    /* TEST_BODY */
+  });
+});
